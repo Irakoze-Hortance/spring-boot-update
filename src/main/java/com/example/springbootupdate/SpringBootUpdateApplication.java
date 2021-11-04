9bc899931949394ac655b030aee24f187cd5643c
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-04 14:02:43 -0700

    initial commit

---
+++ src/main/java/com/example/springbootupdate/SpringBootUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUpdateApplication.class, args);
+    }
+
+}
