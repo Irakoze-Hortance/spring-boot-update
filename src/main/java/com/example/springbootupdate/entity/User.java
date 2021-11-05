45e2d7cbe61cffc5d8cc24ef2b81048d2910a6b1
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-04 19:57:16 -0700

    added user model

---
+++ src/main/java/com/example/springbootupdate/entity/User.java
@@ -0,0 +1,68 @@
+package com.example.springbootupdate.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@Entity(name="Users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    @Builder.Default
+    private UserRole userRole=UserRole.USER;
+    @Builder.Default
+    private Boolean locked=false;
+    @Builder.Default
+    private Boolean enabled=false;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+                        final SimpleGrantedAuthority simpleGrantedAuthority=new SimpleGrantedAuthority(userRole.name());
+                        return Collections.singletonList(simpleGrantedAuthority);
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
