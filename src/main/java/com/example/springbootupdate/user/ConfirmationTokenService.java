69b307df56ccbfc3fdab83cbd5c76018d583d281
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-04 22:07:01 -0700

    added missing methods

--- src/main/java/com/example/springbootupdate/user/ConfirmationTokenService.java
+++ src/main/java/com/example/springbootupdate/user/ConfirmationTokenService.java
@@ -4,11 +4,23 @@ import com.example.springbootupdate.Repository.ConfirmationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class ConfirmationTokenService {
     private final ConfirmationTokenRepository confirmationTokenRepository;
     void saveConfirmationToken(ConfirmationToken confirmationToken){
         confirmationTokenRepository.save(confirmationToken);
+    }
+    void deleteConfirmationToken(Long id) {
+
+        confirmationTokenRepository.deleteById(id);
+    }
+
+
+    Optional<ConfirmationToken> findConfirmationTokenByToken(String token) {
+
+        return confirmationTokenRepository.findConfirmationTokenByConfirmationToken(token);
     }
 }
