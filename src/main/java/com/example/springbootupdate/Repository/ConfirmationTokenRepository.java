e9146a34c4c75804e25999f65210b9bc7703f526
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-04 20:35:29 -0700

    added confirmation token repository

---
+++ src/main/java/com/example/springbootupdate/Repository/ConfirmationTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.springbootupdate.Repository;
+
+import com.example.springbootupdate.user.ConfirmationToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken,Long> {
+}
