121a0e6a1348da7ee731561f5426bb528c4abf45
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-04 22:03:09 -0700

    final ConfirmationToken

--- src/main/java/com/example/springbootupdate/user/ConfirmationToken.java
+++ src/main/java/com/example/springbootupdate/user/ConfirmationToken.java
@@ -1,24 +1,38 @@
 package com.example.springbootupdate.user;
 
 import com.example.springbootupdate.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConfirmationToken {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String confirmationToken;
+
     private LocalDate createdDate;
-    @OneToOne(targetEntity = User.class,fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false,name = "user_id")
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
-    ConfirmationToken(User user){
-        this.user=user;
-        this.createdDate=LocalDate.now();
-        this.confirmationToken= UUID.randomUUID().toString();
+    ConfirmationToken(User user) {
+        this.user = user;
+        this.createdDate = LocalDate.now();
+        this.confirmationToken = UUID.randomUUID().toString();
     }
 }
+
